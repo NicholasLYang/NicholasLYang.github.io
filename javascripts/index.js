fa0d6a920fdf0f522beebbe1670cb4d8a9af519b
Nicholas Yang <nick@nicholasyang.com>
2017-02-26 01:17:14 -0500

    okay more css tweaks

--- javascripts/index.js
+++ javascripts/index.js
@@ -2,8 +2,10 @@ $(window).scroll(function() {
     if ($(document).scrollTop() > 50) {
         $('div.title').addClass('shrink');
         $('span.header-bar').addClass('hide');
+        $('header').addClass('shrink');
     } else {
         $('div.title').removeClass('shrink');
         $('span.header-bar').removeClass('hide');
+        $('header').removeClass('shrink');
     }
 });
